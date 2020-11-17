5e5ca2b40c67aab22bc4a28b72129b9d3fa083df
GeekyAndDangerous <gazby.jones@gmail.com>
2020-11-17 20:16:37 +1100

    Dependencies and git hash information updated

--- nodeAPI.js
+++ nodeAPI.js
@@ -1,14 +1,16 @@
+//Variable declarations
 const express = require('express');
 const app = express();
 const port = 8000;
 const json = require('./package.json');
-var name = "Node API";
+
+var git = require('git-rev-sync');
 
 app.listen(port, ()=> {
-	console.log('listening on port 8000. Access port localhost:8000\\health');
+	console.log('listening on port 8000. Access port: localhost:8000\\health');
 })
 
-
+//The application 
 app.get('/health', function (req,res) {
-res.send('App Name = ' + json.name + '<br/>' + 'Version = ' + json.version)
+res.send('App Name = ' + json.name + '<br/>' + 'Version = ' + json.version + '<br/>' + 'Git hash = ' + git.long())
 })
